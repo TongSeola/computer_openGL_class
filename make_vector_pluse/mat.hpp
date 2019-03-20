28cc8a275bffd36df13d6eaa7662f4133ecee0f9
TongSeola <41602422+TongSeola@users.noreply.github.com>
2019-03-20 12:05:26 +0900

    first homework
    just runing main.cpp

---
+++ make_vector_pluse/mat.hpp
@@ -0,0 +1,122 @@
+#ifndef KMUVCL_GRAPHICS_MAT_HPP
+#define KMUVCL_GRAPHICS_MAT_HPP
+
+#include <iostream>
+#include <cstring>
+#include <cstdarg>
+
+namespace kmuvcl {
+  namespace math {
+
+    template <unsigned int M, unsigned int N, typename T>
+    class mat
+    {
+    public:
+      mat()
+      {
+        set_to_zero();
+      }
+
+      mat(const T elem)
+      {
+        std::fill(val, val + M*N, elem);
+      }
+
+      T& operator()(unsigned int r, unsigned int c)
+      {
+        return val[r+c*M];
+      }
+
+      const T& operator()(unsigned int r, unsigned int c) const
+      {
+        // TODO: Fill up this function properly 
+        // Notice: The matrix is column major
+        return  val[0];   
+      }
+
+      // type casting operators
+      operator const T* () const
+      {
+        return  val;
+      }
+
+      operator T* ()
+      {
+        return  val;
+      }
+
+      void set_to_zero()
+      {
+        int MaxIndex = M*N;
+        for(int i=0; i< MaxIndex; ++i)
+        {
+          val[i] = 0;
+        }
+      }
+
+      void get_ith_column(unsigned int i, vec<M, T>& col) const
+      {
+        int k = 0;
+        int startIndex = i*M;
+        int endIndex = i*M+M;
+    
+        for(int j=startIndex; j < endIndex; ++j)
+        {
+          col[k] = val[j]; 
+          ++k;
+        }
+      }
+
+      void set_ith_column(unsigned int i, const vec<M, T>& col)
+      {
+        // TODO: Fill up this function properly 
+      }
+
+      void get_ith_row(unsigned int i, vec<N, T>& row) const
+      {
+        int k = 0;
+        int startIndex = i;
+        int endIndex = i+N*N;
+        //  std::cout << "startindex, endindex: "<< startIndex<< " " << endIndex<< std::endl;
+        for(int j=startIndex; j < endIndex; j+=N)
+        {
+          // std::cout << "row[" << k << "]="<< row[k] << std::endl;
+          row[k] = val[j]; 
+          ++k;
+        }
+      }
+
+      void set_ith_row(unsigned int i, const vec<N, T>& row)
+      {
+        // TODO: Fill up this function properly 
+      }
+
+      mat<N, M, T> transpose() const
+      {
+        mat<N, M, T>  trans;
+        for(int i=0; i<N;++i)
+        {
+          for(int j=0; j< M; ++j)
+          {
+            trans(i,j) = this->val[i*M+j];
+          }
+        }        
+        return  trans;
+      }
+
+    protected:
+      T val[M*N];   // column major
+    };
+
+    typedef mat<3, 3, float>    mat3x3f;
+    typedef mat<3, 3, double>   mat3x3d;
+
+    typedef mat<4, 4, float>    mat4x4f;
+    typedef mat<4, 4, double>   mat4x4d;
+
+  } // math
+} // kmuvcl
+
+#include "operator.hpp"
+
+#endif // #ifndef KMUVCL_GRAPHICS_MAT_HPP
